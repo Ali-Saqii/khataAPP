c780611e8aac4acb2cdfa786adc41f5f05049cf9
Ali Saqii <mr.sadaqatalisaqii@gmail.com>
2025-12-30 20:31:27 +0500

    Initial Commit

---
+++ khata/khataApp.swift
@@ -0,0 +1,17 @@
+//
+//  khataApp.swift
+//  khata
+//
+//  Created by Mac mini on 30/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct khataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ khataTests/khataTests.swift
@@ -0,0 +1,17 @@
+//
+//  khataTests.swift
+//  khataTests
+//
+//  Created by Mac mini on 30/12/2025.
+//
+
+import Testing
+@testable import khata
+
+struct khataTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
